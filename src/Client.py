14019fdd668bdfc65d0519927d50b1d497151404
ZeidmanRon <ronze@post.bgu.ac.il>
2020-12-28 14:03:10 +0200

    TCP CLinet,Server and UDP Client,Server are firstly implemented

--- src/Client.py
+++ src/Client.py
@@ -1,15 +1,35 @@
-import socket
+from socket import *
+from keyboard import *
 
 
 class Client:
 
-    def __init__(self, port):
-        self.ip = socket.gethostbyname(socket.gethostname())
+    def __init__(self, port = 13117):
+        self.ip = gethostbyname(gethostname())
         self.port = port
 
 
-    def lookingForServer(self):
-        pass
+    def UDPClient(self):
+        serverName = 'hostname'
+        serverPort = self.port
+        clientSocket = socket(AF_INET, SOCK_DGRAM)
+        message = input('Hello im Client')
+        clientSocket.sendto(message, (serverName, serverPort))
+        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+        print(modifiedMessage)
+        clientSocket.close()
+
+    def TCPClient(self):
+        serverName = 'servername'
+        serverPort = self.port
+        clientSocket = socket(AF_INET, SOCK_DGRAM)
+        message = input('Hello im Client')
+        clientSocket.connect((serverName, serverPort))
+        sentence = input('name of my group is QueenGambit')
+        clientSocket.send(sentence)
+        modifiedSentence = clientSocket.recv(1024)
+        print('from server: ', modifiedSentence)
+        clientSocket.close()
 
 
     def connetcingToServer(self):
--- src/Server.py
+++ src/Server.py
@@ -1,23 +1,23 @@
 from src.Client import *
 from socket import *
 
+
 class Server:
 
     def __init__(self):
         self.ip = socket.gethostbyname(socket.gethostname())
-        self.port=13117
+        self.port = 13117
 
     def UDPServer(self):
         serverPort = self.port
-        serverSocket = socket(AF_INET,SOCK_DGRAM)
-        serverSocket.bind(('',serverPort))
+        serverSocket = socket(AF_INET, SOCK_DGRAM)
+        serverSocket.bind(('', serverPort))
         print("the server ready to recieve")
         while 1:
-            massege, clientAdd=serverSocket.recvfrom(2048)
-            #fuction that the serverUDP do ********
-            modifiedMessage=massege.upper()
-            serverSocket.sendto(modifiedMessage,clientAdd)
-
+            massege, clientAdd = serverSocket.recvfrom(2048)
+            # fuction that the serverUDP do ********
+            modifiedMessage = massege.upper()
+            serverSocket.sendto(modifiedMessage, clientAdd)
 
     def TCPServer(self):
         serverPort = self.port
@@ -29,5 +29,5 @@ class Server:
             connectionSocket, addr = serverSocket.accept()
 
             sentence = connectionSocket.recv(1024)
-            capitalizeSentence=sentence.upper()
+            capitalizeSentence = sentence.upper()
             connectionSocket.close()
